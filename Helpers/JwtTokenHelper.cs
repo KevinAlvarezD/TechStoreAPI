4a0756c3b0178cfa56478c25232ac0a51cef3477
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-10-05 13:49:11 -0500

    feat: Create Token in Helpers

--- Helpers/JwtTokenHelper.cs
+++ Helpers/JwtTokenHelper.cs
@@ -1,11 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
-namespace TechStoreAPI.Helpers;
-
-public class JwtTokenHelper
+namespace TechStore.Helpers
 {
+    public static class JwtTokenHelper
+    {
+        public static string GenerateToken(string username, string role, string secretKey)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(secretKey);
 
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.Role, role)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
 }
+
